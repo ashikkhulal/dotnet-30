59c4d5893c3bb9012af6438768269b08488d1f13
ashikkhulal <askhulal@gmail.com>
2023-06-22 17:30:43 -0500

    Added day 3 assignment and updated readme file

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # dotnet-class-notes-and-projects
 
-## [DAY-1](./day_1/class_lessons/)
+## [DAY-1](./day_1/class_lesson/)
 
 Day 1 Topics are:
 
@@ -10,7 +10,7 @@ Day 1 Topics are:
 
 Day 1 Project: None
 
-## [DAY-2](./day_2/class_lessons/)
+## [DAY-2](./day_2/class_lesson/)
 
 Day 2 Topics are:
 
@@ -21,4 +21,16 @@ Day 2 Topics are:
 - User input into variables
 - Data types conversion
 
-Day 2 Project: [Simple Calculator](./day_2/assignments/)
+Day 2 Project: [Simple Calculator](./day_2/assignment/)
+
+## [DAY-3](./day_3/class_lesson/)
+
+Day 3 Topics are:
+
+- Class
+- Objects
+- Properties
+- Encapsulation
+- Methods or Functions
+
+Day 3 Project: [Simple Calculator](./day_3/assignment/)
--- day_3/assignment/simpleCalculator/simpleCalculator/Program.cs
+++ day_3/assignment/simpleCalculator/simpleCalculator/Program.cs
@@ -6,7 +6,103 @@ namespace simpleCalculator
     {
         static void Main(string[] args)
         {
+            //myCalc object declaration
+            Calculator myCalc = new Calculator();
 
+            //calling welcomeDialogue method
+            myCalc.welcomeDialogue();
+            //setting FirstNumber property value by calling getFirstNumbers method
+            myCalc.FirstNumber = myCalc.getFirstNumbers();
+            //setting SecondNumber property value by calling SecondNumber method
+            myCalc.SecondNumber = myCalc.getSecondNumbers();
+            //setting Choice property value by calling operationChoice method
+            myCalc.Choice = myCalc.operationChoice();
+            //calling calculateAnswer method
+            myCalc.calculateAnswer();
+
+            Console.ReadKey();
+
+        }
+
+        //class calculator
+        class Calculator
+        {
+            //private properties
+            private double _FirstNumber;
+            private double _SecondNumber;
+            private int _Choice;
+            private double _Answer;
+
+            //public properties
+            public double FirstNumber { get { return _FirstNumber; } set { _FirstNumber = value; } }
+            public double SecondNumber { get { return _SecondNumber; } set { _SecondNumber = value; } }
+            public int Choice { get { return _Choice; } set { _Choice = value; } }
+            public double Answer { get { return _Answer; } set { _Answer = value; } }
+
+            //welcomeDialogue method
+            public void welcomeDialogue()
+            {
+                Console.WriteLine("Welcome to a simple calculator!");
+                Console.WriteLine("Give me two numbers and I will either add, subtract, multiply, or divide them for you.\n");
+            }
+
+            //method to get and return first numbers from user
+            public double getFirstNumbers()
+            {
+                Console.Write("Enter your first number: ");
+                this.FirstNumber = Convert.ToDouble(Console.ReadLine());
+
+                return FirstNumber;
+            }
+
+            //method to get and return second numbers from user
+            public double getSecondNumbers()
+            {
+                Console.Write("Enter your Second number: ");
+                this.SecondNumber = Convert.ToDouble(Console.ReadLine());
+
+                return SecondNumber;
+            }
+
+            //method to get and return operation choice
+            public int operationChoice()
+            {
+                Console.WriteLine("\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n");
+                Console.Write("What operation would you like to perform? (Example 1 to add or 2 to subtract and so on: ");
+                this.Choice = Convert.ToInt16(Console.ReadLine());
+
+                return Choice;
+            }
+
+            //method to calculate and return answer
+            public void calculateAnswer()
+            {
+                if (Choice == 1)
+                {
+                    Answer = FirstNumber + SecondNumber;
+                    Console.WriteLine("You want to add " + FirstNumber + " and " + SecondNumber + ". And the answer is: " + Answer);
+                }
+                else if (Choice == 2)
+                {
+                    Answer = SecondNumber - FirstNumber;
+                    Console.WriteLine("You want to subtract " + FirstNumber + " from " + SecondNumber + ". And the answer is: " + Answer);
+                }
+                else if (Choice == 3)
+                {
+                    Answer = FirstNumber * SecondNumber;
+                    Console.WriteLine("You want to multiply " + FirstNumber + " and " + SecondNumber + ". And the answer is: " + Answer);
+                }
+                else if (Choice == 4)
+                {
+                    Answer = FirstNumber / SecondNumber;
+                    Console.WriteLine("You want to divide " + FirstNumber + " by " + SecondNumber + ". And the answer is: " + Answer);
+                }
+                else
+                {
+                    Console.WriteLine("You entered an invalid choice! Try next time.");
+                    
+                }
+            }
         }
     }
 }
