5acfd658eba18074dcfdc4aa8c799299e60eabbb
ashikkhulal <askhulal@gmail.com>
2023-06-22 16:36:20 -0500

    added day 3 materials

---
+++ day_3/assignment/simpleCalculator/simpleCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿// Simple calculator (add, subtract, multiply, and divide) with the usage of class, method, objects and properties
+
+namespace simpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
