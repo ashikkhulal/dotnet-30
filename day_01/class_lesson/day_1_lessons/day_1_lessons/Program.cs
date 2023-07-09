3af378281f935f39f7cffa5c92bf7f9a2d65f242
ashikkhulal <askhulal@gmail.com>
2023-07-09 12:20:58 -0500

    Updated day 1 lessons

--- day_01/class_lesson/day_1_lessons/day_1_lessons/Program.cs
+++ day_01/class_lesson/day_1_lessons/day_1_lessons/Program.cs
@@ -18,6 +18,15 @@ namespace day_1_lessons
     //Class name
 	class Program
 	{
+        //Day 1 topics:
+        //1. Syllabus
+        //2. C# introduction
+        //3. C# keywords
+        //4. Console application introduction
+
+
+        //C# introduction:
+
         //C# Version History:
         //C# was first introduced with .NET Framework 1.0 in the year 2002 and evolved much since then. The following table lists important features introduced in each version of C#
 
@@ -42,6 +51,47 @@ namespace day_1_lessons
             string message = "Hello, World!";
             Console.WriteLine(message);
 		}
+
+        //C# keywords:
+
+        //C# contains reserved words that have special meaning for the compiler. These reserved words are called "keywords".
+        //Keywords cannot be used as an identifier (name of a variable, class, interface, etc.)
+        //Keywords in C# are distributed under the following categories:
+
+        //A. Modifier keywords: they are specific keywords that indicate who can modify types and type members. Modifiers allow or prevent certain parts of programs from being modified by other parts.
+        //Modifier keywords are: abstract, async, const, event, extern, new, override, partial, readonly, sealed, static, unsafe, virtual, volatile
+
+        //B. Access modifier keywords: they are applied to the declaration of the class, method, properties, fields, and other members. They define the accessibility of the class and its members.
+        //Access modifier keywords are: public, private, internal, protected
+
+        //C. Statement keywords: they are related to program flow.
+        //Statement keywords are: if, else, switch, case, do, for, foreach, in, while, break, continue, default, goto, return, yield, throw, try, catch, finally, checked, unchecked, fixed, lock
+
+        //D. Method parameter keywords: these keywords are applied to the parameters of a method.
+        //Method parameter keywords are: params, ref, out
+
+        //E. Namespace keywords: these keywords are applied with namespace and related operators.
+        //Namespace keywords are: using, . operator, :: operator, extern alias
+
+        //F. Operator keywords: they perform miscellaneous actions.
+        //Operator keywords are: as, await, is, new, sizeof, typeof, stackalloc, checked, unchecked
+
+        //G. Access keywords: they are used to access the containing class or the base class of an object or class.
+        //Access keywords are: base, this
+
+        //H. Literal keywords: they apply to the current instance or value of an object.
+        //Literal keywords are: null, false, true, value, void
+
+        //I. Type keywords: they are used for data types.
+        //Type keywords are: bool, byte, char, class, decimal, double, enum, float, int, long, sbyte, short, string, struct, uint, uling, ushort
+
+        //J. Contextual keywords: these keywords are considered as keywords, only if used in specific contexts. They are not reserved and so can be used as names or identifiers.
+        //Contextual keywords are: add, var, dynamic, global, set, value
+
+        //K. Query keywords: query keywords are contextual keywords used in LINQ queries.
+        //Query keywords are: from, where, select, group, into, orderby, join, let, in, on, equals, by, ascending, descending
+
+        //Console application introduction
 
         //Let us understand the above C# structure:
         //1. Every .NET application takes the reference of the necessary .NET framework namespaces that it is planning to use with the using keyword, e.g., using System.Text.
